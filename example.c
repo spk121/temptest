94de7656d55aef07de524796e843143a88d3ead3
Mike Gran <spk121@yahoo.com>
2024-01-21 08:04:36 -0800

    Update example.c

--- example.c
+++ example.c
@@ -205,3 +205,117 @@ void runTests(void)
 	for (int i = 0; i < N_TESTS; i++)
 		(testList[i])();
 }
+
+
+#define XSTR(x) STR(x)
+#define STR(x) #x
+
+#define DECL_COUNT(fname) extern int mock_##fname##_count
+#define DEF_COUNT(fname) int mock_##fname##_count
+#define COUNT(fname) mock_##fname##_count++
+
+DECL_COUNT(foo);
+DEF_COUNT(foo);
+
+#define DECL_RET(fname,type) extern type mock_##fname##_return
+#define DEF_RET(fname,type) type mock_##fname##_return
+#define RET(fname) \
+    return mock_##fname##_return;
+
+void foo(void)
+{
+	COUNT(foo);
+	printf("%s:%d", __func__, __LINE__);
+}
+
+DECL_RET(int, foo2);
+DEF_RET(int, foo2);
+DECL_COUNT(foo2);
+DEF_COUNT(foo2);
+
+int foo2(void)
+{
+	COUNT(foo2);
+	RET(foo2);
+}
+
+#define DECL_IN(func,type,arg) \
+  extern type mock_##func##_##arg
+
+#define DEF_IN(func,type,arg) \
+  type mock_##func##_##arg
+
+#define IN(func, arg) \
+  mock_##func##_##arg = (arg)
+
+DEF_IN(foo3, int, x);
+DEF_IN(foo3, const char*, str);
+
+void foo3(int x, const char *str)
+{
+	IN(foo3, x);
+	IN(foo3, str);
+}
+
+#define DECL_OUT(func,type,arg) \
+  extern type mock_##func##_##arg
+
+#define DEF_OUT(func,type,arg) \
+  type mock_##func##_##arg
+
+#define OUT(func,arg) \
+    *arg = mock_##func##_##arg
+
+DECL_OUT(foo4, int, x);
+
+DEF_OUT(foo4, int, x);
+
+void foo4(int* x)
+{
+	OUT(foo4,x);
+}
+
+#define DECL_CB(func,type,...) \
+  extern type (*mock_##func##_cb)(__VA_ARGS__)
+#define DEF_CB(func,type,...) \
+  type (*mock_##func##_cb)(__VA_ARGS__)
+
+#define CB(funcName,...) \
+  do { \
+    if(mock_##funcName##_cb) \
+      return mock_##funcName##_cb(__VA_ARGS__); \
+  } while(0)
+
+#define DECL_VCB(func,...) \
+  extern void (*mock_##func##_cb)(__VA_ARGS__)
+#define DEF_VCB(func,...) \
+  void (*mock_##func##_cb)(__VA_ARGS__)
+
+#define VCB(func,...) \
+  do { \
+    if(mock_##func##_cb) \
+      mock_##func##_cb(__VA_ARGS__); \
+  } while(0)
+
+DEF_COUNT(bar);
+DEF_VCB(bar, void);
+
+void bar(void)
+{
+	COUNT(bar);
+	VCB(bar);
+}
+
+DEF_COUNT(bar2);
+DEF_CB(bar2, int, int x, float y);
+DEF_IN(bar2, int, x);
+DEF_IN(bar2, float, y);
+DEF_RET(bar2, int);
+int bar2(int x, float y)
+{
+	COUNT(bar);
+	CB(bar2, x, y);
+	IN(bar2, x);
+	IN(bar2, y);
+	RET(bar2);
+}
