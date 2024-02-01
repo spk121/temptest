049d84cc9656d90506f394cb79052c99bc8a2525
Mike Gran <spk121@yahoo.com>
2024-02-01 07:03:37 -0800

    Update EventLog.c

--- EventLog.c
+++ EventLog.c
@@ -1,15 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <string.h>
 #include <stdbool.h>
 #include "EventLog.h"
 
 
-#define STX 2
-#define ETX 3
+#define STX '['
+#define ETX ']'
 #define ESC 27
 #define OFFSET 32
-
-// bytes in an event packet
-#define PACKET_SIZE 10
 
 // In the worst case, every byte in the packet requires an escape
 // character plus starting and ending framing characters. 
@@ -34,19 +32,17 @@ typedef enum EventPayload {
 
 typedef struct Packet
 {
-    uint16_t version : 2;
-    uint16_t level : 2;
-    uint16_t format : 4;
-    uint16_t source : 8;
+    uint8_t version : 4;
+    uint8_t level : 4;
+    uint8_t format;
+    uint8_t source;
+    uint8_t type;
 
-    uint16_t type : 8;
-    uint16_t timestampHigh : 8;
-
-    uint16_t timestampLow;
+    uint32_t timestamp;
 
     union {
         uint16_t words[2];
-        bool boolean;
+        int8_t boolean;
         int8_t s8;
         uint8_t u8;
         int16_t s16;
@@ -57,6 +53,9 @@ typedef struct Packet
         char str[4];
     };
 } Packet;
+
+// bytes in an event packet
+#define PACKET_SIZE sizeof(Packet)
 
 static void setHeader(Packet *p, EventLevel level, EventSource source, EventType type)
 {
@@ -71,8 +70,7 @@ static void setHeader(Packet *p, EventLevel level, EventSource source, EventType
     p->source = source;
     p->type = type;
     p->format = PAYLOAD_NONE;
-    p->timestampHigh = (timestamp & 0x00FF0000) >> 16;
-    p->timestampLow = (timestamp & 0xFFFF);
+    p->timestamp = timestamp;
 }
 
 static void sendPacket(const char* buf, int len)
@@ -116,12 +114,70 @@ static void sendPacket(const char* buf, int len)
     }
     frame[n] = ETX;
     n++;
+
     itsOutputFunc(frame, n);
 }
 
-static bool unpackFrame(const char* frame, int size, Packet* pkt, int *bytesRead)
+static EventData convertPacketToEvent(Packet* pkt)
 {
-    char buf[PACKET_SIZE] = { 0 };
+    EventData event = { 0 };
+    event.level = pkt->level;
+    event.sourceID = pkt->source;
+    event.eventID = pkt->type;
+    event.timestamp = pkt->timestamp;
+    switch (pkt->format)
+    {
+    case PAYLOAD_BOOLEAN:
+        event.dataType = EVENT_DATA_BOOL;
+        event.data.boolean = pkt->boolean;
+        break;
+    case PAYLOAD_INT8:
+        event.dataType = EVENT_DATA_INT8;
+        event.data.s8 = pkt->s8;
+        break;
+    case PAYLOAD_UINT8:
+        event.dataType = EVENT_DATA_UINT8;
+        event.data.u8 = pkt->u8;
+        break;
+    case PAYLOAD_INT16:
+        event.dataType = EVENT_DATA_INT16;
+        event.data.s16 = pkt->s16;
+        break;
+    case PAYLOAD_UINT16:
+        event.dataType = EVENT_DATA_UINT16;
+        event.data.u16 = pkt->u16;
+        break;
+    case PAYLOAD_INT32:
+        event.dataType = EVENT_DATA_INT32;
+        event.data.s32 = pkt->s32;
+        break;
+    case PAYLOAD_UINT32:
+        event.dataType = EVENT_DATA_UINT32;
+        event.data.u16 = pkt->u32;
+        break;
+    case PAYLOAD_FLOAT:
+        event.dataType = EVENT_DATA_FLOAT;
+        event.data.f32 = pkt->f32;
+        break;
+    case PAYLOAD_STRING:
+        event.dataType = EVENT_DATA_STRING;
+        event.data.str[0] = pkt->str[0];
+        event.data.str[1] = pkt->str[1];
+        event.data.str[2] = pkt->str[2];
+        event.data.str[3] = pkt->str[3];
+        break;
+    case PAYLOAD_NONE:
+    default:
+        event.dataType = EVENT_DATA_NONE;
+        event.data.u32 = 0;
+        break;
+    }
+    return event;
+}
+
+EventData eventUnpackFrame(const char* frame, int size)
+{
+    char buf[sizeof(Packet)] = {0};
     bool startFound = false;
     bool endFound = false;
     bool escFound = false;
@@ -129,7 +185,7 @@ static bool unpackFrame(const char* frame, int size, Packet* pkt, int *bytesRead
     int packetPos = 0;
     int framePos = 0;
 
-    while (framePos < size && packetPos < PACKET_SIZE && !errorFound)
+    while (framePos < size && !errorFound)
     {
         if (!startFound && frame[framePos] == STX)
         {
@@ -143,7 +199,7 @@ static bool unpackFrame(const char* frame, int size, Packet* pkt, int *bytesRead
                 (frame[framePos] != ETX + OFFSET) &&
                 (frame[framePos] != ESC + OFFSET))
             {
-                errorFound = true;;
+                errorFound = true;
             }
             buf[packetPos] = frame[framePos] - OFFSET;
             packetPos++;
@@ -168,16 +224,24 @@ static bool unpackFrame(const char* frame, int size, Packet* pkt, int *bytesRead
         }
     }
 
-    *bytesRead = framePos;
-
-    if (packetPos == PACKET_SIZE && startFound && endFound)
+    if (packetPos == sizeof(Packet) && startFound && endFound)
     {
+        Packet pkt;
+        EventData eventGood;
         // May need to byteswap here.
-        memcpy((void*)pkt, buf, PACKET_SIZE);
-        return true;
+        memcpy((void*)&pkt, buf, sizeof(Packet));
+        eventGood = convertPacketToEvent(&pkt);
+        eventGood.frameSize = framePos;
+        eventGood.valid = true;
+        return eventGood;
     }
-
-    return false;
+    else
+    {
+        EventData eventBad = { 0 };
+        eventBad.frameSize = framePos;
+        eventBad.valid = false;
+        return eventBad;
+    }
 }
 
 void eventSetOutputFunc(EventOutputFunc outputFunc)
@@ -194,120 +258,107 @@ void event(EventLevel level, EventSource source, EventType type)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    sendPacket((const char *) & p, sizeof(p));
 }
 
 void eventBool(EventLevel level, EventSource source, EventType type, bool val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.boolean = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_BOOLEAN;
+    p.boolean = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventU8(EventLevel level, EventSource source, EventType type, uint8_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.u8 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_UINT8;
+    p.u8 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventS8(EventLevel level, EventSource source, EventType type, int8_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.s8 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_INT8;
+    p.s8 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventU16(EventLevel level, EventSource source, EventType type, uint16_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.u16 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_UINT16;
+    p.u16 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventS16(EventLevel level, EventSource source, EventType type, int16_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.s16 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_INT16;
+    p.s16 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventU32(EventLevel level, EventSource source, EventType type, uint32_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.u32 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_UINT32;
+    p.u32 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventS32(EventLevel level, EventSource source, EventType type, int32_t val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.s32 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_INT32;
+    p.s32 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventFloat(EventLevel level, EventSource source, EventType type, float val)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
-    {
-        setHeader(&p, level, source, type);
-        p.f32 = val;
-        itsOutputFunc(&p, sizeof(p));
-    }
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_FLOAT;
+    p.f32 = val;
+    sendPacket((const char*)&p, sizeof(p));
 }
 
 void eventStr(EventLevel level, EventSource source, EventType type, const char *str)
 {
     Packet p = { 0 };
 
-    if (itsOutputFunc)
+    setHeader(&p, level, source, type);
+    p.format = PAYLOAD_STRING;
+    if (str)
     {
-        setHeader(&p, level, source, type);
-        if (str)
-        {
-            strncpy(p.str, str, 4);
-        }
-        itsOutputFunc(&p, sizeof(p));
+        strncpy(p.str, str, 4);
     }
+    else
+    {
+        p.str[0] = 0;
+        p.str[1] = 0;
+        p.str[2] = 0;
+        p.str[3] = 0;
+    }
+    sendPacket((const char *) & p, sizeof(p));
 }
+
